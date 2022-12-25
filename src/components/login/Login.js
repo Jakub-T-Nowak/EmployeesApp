474c77a22b62a8bda0e609f485c56e2934bdb865
Jakub <jnow87@gmail.com>
2022-12-25 18:58:09 +0100

    J-18: log in option added

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,32 +1,46 @@
-import React, { useRef } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import FirebaseAuthService from "../../FirebaseAuthService";
 import styles from "./Login.module.sass";
 
 const LogIn = () => {
+    const [log, setLog] = useState(false);
     const userNameRef = useRef();
     const passwordRef = useRef();
 
-    const handleSubmit = (event) => {
+    const handleSubmit = event => {
         event.preventDefault();
 
         const userName = userNameRef.current.value;
         const password = passwordRef.current.value;
 
-        FirebaseAuthService.registerUser(userName, password)
-            .then((userCredential) => {
+        const firebaseAction = log
+            ? FirebaseAuthService.loginUser
+            : FirebaseAuthService.registerUser;
+
+        firebaseAction(userName, password)
+            .then(userCredential => {
                 console.log(userCredential);
             })
-            .catch((e) => console.log(e.message));
+            .catch(e => console.log(e.message));
+    };
+
+    const change = () => {
+        setLog(prev => !prev);
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label className={styles.login__label}>User</label>
-            <input type="text" ref={userNameRef} />
-            <label>Login</label>
-            <input type="text" ref={passwordRef} />
-            <button type="submit">Log in</button>
-        </form>
+        <Fragment>
+            <button type="button" onClick={change}>
+                {!log ? "Log in" : "Sign in"}
+            </button>
+            <form onSubmit={handleSubmit}>
+                <label className={styles.login__label}>User</label>
+                <input type="text" ref={userNameRef} />
+                <label>Login</label>
+                <input type="text" ref={passwordRef} />
+                <button type="submit">{log ? "Log in" : "Sign in"}</button>
+            </form>
+        </Fragment>
     );
 };
 
